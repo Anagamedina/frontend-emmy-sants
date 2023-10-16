4fa20252fb1a27e7a07070062f51a9cad3eadf4d
Ana Gabriela Medina <anaga.medina@gmail.com>
2023-10-16 12:42:43 +0200

    profileintentando

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,203 +1,3 @@
-
-//alejandro
-
-
-// import React, { useState, useEffect } from 'react';
-// import authService from "../../services/auth.service";
-// import { Link } from "react-router-dom"; // Agrega el import para el componente Link
-// import './ProfilePage.css';
-// import Card from 'react-bootstrap/Card';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
-
-// function ProfilePage() {
-//   const [orders, setOrders] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     async function fetchOrderHistory() {
-//       try {
-//         const response = await authService.api.get("/api/orders/history");
-
-//         response.data.forEach(order => {
-//           let totalAmount = 0;
-//           order.products.forEach(productInfo => {
-//             totalAmount += productInfo.amount * (productInfo.product ? productInfo.product.precio : 0);
-//           });
-//           order.totalAmount = totalAmount;
-//         });
-
-//         setOrders(response.data);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Error fetching order history:", error);
-//         setLoading(false);
-//       }
-//     }
-
-//     fetchOrderHistory();
-//   }, []);
-
-//   return (
-//     <div className="profile-page">
-//       <h1>Profile Page</h1>
-
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <Row className="justify-content-center">
-//           {orders.map((order) => (
-//             <Col key={order._id} sm={12} md={4} lg={3} xl={3} className="mb-3">
-//               <Card className="card h-100 bg-white rounded">
-//                 <Card.Header className='cardHeader' >
-//                   <strong>Fecha del Pedido:<br/></strong> {order.createdAt ? new Date(order.createdAt).toLocaleString() : 'N/A'}
-//                 </Card.Header>
-//                 <Card.Body className='cardBody' >
-//                   <Card.Title className='cardTitle' >Pedido</Card.Title>
-//                   <ul>
-//                     {order.products.map((productInfo, index) => (
-//                       <li key={index}>
-//                         <div className="product-info">
-//                           {productInfo.product && (
-//                             <Link to={`/product/plantas/${productInfo.product._id}`}> {/* Agregar Link a la página de detalles del producto */}
-//                               <img
-//                                 className="product-image"
-//                                 src={productInfo.product.imagen}
-//                                 alt={productInfo.product.nombre}
-//                               />
-//                             </Link>
-//                           )}
-//                           <div className="product-name">
-//                             {productInfo.amount} x{' '}
-//                             {productInfo.product && (
-//                               <Link to={`/product/plantas/${productInfo.product._id}`}>
-//                                 <span style={{ textDecoration: 'none', color: '#4abcf1' }}>{productInfo.product.nombre}</span>
-//                               </Link>
-//                             ) || 'N/A'} 
-//                           </div>
-//                           <div className="product-price">
-//                             Precio por unidad: {productInfo.product && productInfo.product.precio || 0}€
-//                           </div>
-//                           <div className="product-total">
-//                             Total: {productInfo.amount * (productInfo.product && productInfo.product.precio || 0)}€
-//                           </div>
-//                         </div>
-//                       </li>
-//                     ))}
-//                   </ul>
-//                 </Card.Body>
-//                 <Card.Footer className='cardFooter' >
-//                   <strong>Total Pedido:</strong> {order.totalAmount || 0}€
-//                   <br />
-//                   <strong>Estado del Pedido:</strong> {order.state || 'N/A'}
-//                 </Card.Footer>
-//               </Card>
-//             </Col>
-//           ))}
-//         </Row>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default ProfilePage;
-
-
-
-
-
-// ANA 
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import authService from "../../services/auth.service";
-// import './ProfilePage.css';
-
-// function ProfilePage() {
-//   const [orders, setOrders] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [suma, setSuma] = useState(0);
-
-//   useEffect(() => {
-//     async function fetchOrderHistory() {
-//       try {
-//         const response = await authService.api.get("/api/orders/history");
-        
-//         let sumaa 
-//         response.data.map(order=>{
-//           sumaa = 0
-//           order.products.map(productInfo => {
-//             sumaa+= productInfo.amount  *(productInfo.product && productInfo.product.precio || 0)
-//           })
-//           order.totalAmount = sumaa
-//         })
-
-//         setOrders(response.data);
-//         setLoading(false);
-
-//       } catch (error) {
-//         console.error("Error fetching order history:", error);
-//         setLoading(false);
-//       }
-//     }
-
-//     fetchOrderHistory();
-//   }, []);
-
-//   return (
-//     <div className="profile-page">
-//       <h1>Profile Page</h1>
-
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <div className="order-list">
-//           <div className="order-cards">
-//             {orders.map((order) => (
-//               <div className="order-card" key={order._id}>
-//                 <div className="order-date">
-//                   <strong>Order Date:</strong> {order.createdAt ? new Date(order.createdAt).toLocaleString() : 'N/A'}
-//                 </div>
-//                 <div className="order-details">
-//                   <h3>Order Details</h3>
-//                   <ul>
-//                     {order.products.map((productInfo, index) => (
-//                       <li key={index}>
-//                         <div className="product-info">
-//                           {productInfo.product && (
-//                             <img
-//                               className="product-image"
-//                               src={productInfo.product.imagen} 
-//                               alt={productInfo.product.nombre}
-//                             />
-//                           )}
-//                           <div className="product-name">
-//                             {productInfo.amount} x {productInfo.product && productInfo.product.nombre || 'N/A'} (${(productInfo.amount * (productInfo.product && productInfo.product.precio || 0))})
-//                           </div>
-//                         </div>
-//                       </li>
-//                     ))}
-//                   </ul>
-//                 </div>
-//                 <div className="order-total">
-//                   <strong>Total:</strong> ${order.totalAmount || 0}
-//                 </div>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default ProfilePage;
-
-
-
 import React, { useState, useEffect } from 'react';
 import authService from "../../services/auth.service";
 import './ProfilePage.css';
@@ -205,14 +5,25 @@ import './ProfilePage.css';
 function ProfilePage() {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [suma, setSuma] = useState(0);
 
   useEffect(() => {
     async function fetchOrderHistory() {
       try {
         const response = await authService.api.get("/api/orders/history");
+        
+        let sumaa 
+        response.data.map(order=>{
+          sumaa = 0
+          order.products.map(productInfo => {
+            sumaa+= productInfo.amount  *(productInfo.product && productInfo.product.precio || 0)
+          })
+          order.totalAmount = sumaa
+        })
 
         setOrders(response.data);
         setLoading(false);
+
       } catch (error) {
         console.error("Error fetching order history:", error);
         setLoading(false);
@@ -222,16 +33,10 @@ function ProfilePage() {
     fetchOrderHistory();
   }, []);
 
-  // Verificar si el usuario está autenticado
-  const isAuthenticated = authService.isAuthenticated();
-
-  if (!isAuthenticated) {
-    return <p>You are not logged in. Please log in to view your profile.</p>;
-  }
-
   return (
     <div className="profile-page">
       <h1>Profile Page</h1>
+
       {loading ? (
         <p>Loading...</p>
       ) : (
@@ -251,7 +56,7 @@ function ProfilePage() {
                           {productInfo.product && (
                             <img
                               className="product-image"
-                              src={productInfo.product.imagen}
+                              src={productInfo.product.imagen} 
                               alt={productInfo.product.nombre}
                             />
                           )}
@@ -276,4 +81,3 @@ function ProfilePage() {
 }
 
 export default ProfilePage;
-
