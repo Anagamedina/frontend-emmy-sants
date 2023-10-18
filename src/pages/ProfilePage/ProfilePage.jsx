55172b436ad02001da42d1d7ebc83cf7e60fb6a3
Ana Gabriela Medina <anaga.medina@gmail.com>
2023-10-18 16:01:51 +0200

    profile

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -39,59 +39,37 @@ function ProfilePage() {
       <h1>Profile Page</h1>
 
       {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <Row className="justify-content-center">
-          {orders.map((order) => (
-            <Col key={order._id} sm={12} md={4} lg={3} xl={3} className="mb-3">
-              <Card className="card h-100 bg-white rounded">
-                <Card.Header className='cardHeader' >
-                  <strong>Fecha del Pedido:<br/></strong> {order.createdAt ? new Date(order.createdAt).toLocaleString() : 'N/A'}
-                </Card.Header>
-                <Card.Body className='cardBody' >
-                  <Card.Title className='cardTitle' >Pedido</Card.Title>
-                  <ul>
-                    {order.products.map((productInfo, index) => (
-                      <li key={index}>
-                        <div className="product-info">
-                          {productInfo.product && (
-                            <Link to={`/product/plantas/${productInfo.product._id}`}> {/* Agregar Link a la página de detalles del producto */}
-                              <img
-                                className="product-image"
-                                src={productInfo.product.imagen}
-                                alt={productInfo.product.nombre}
-                              />
-                            </Link>
-                          )}
-                          <div className="product-name">
-                            {productInfo.amount} x{' '}
-                            {productInfo.product && (
-                              <Link to={`/product/plantas/${productInfo.product._id}`}>
-                                <span style={{ textDecoration: 'none', color: '#4abcf1' }}>{productInfo.product.nombre}</span>
-                              </Link>
-                            ) || 'N/A'} 
-                          </div>
-                          <div className="product-price">
-                            Precio por unidad: {productInfo.product && productInfo.product.precio || 0}€
-                          </div>
-                          <div className="product-total">
-                            Total: {productInfo.amount * (productInfo.product && productInfo.product.precio || 0)}€
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                </Card.Body>
-                <Card.Footer className='cardFooter' >
-                  <strong>Total Pedido:</strong> {order.totalAmount || 0}€
-                  <br />
-                  <strong>Estado del Pedido:</strong> {order.state || 'N/A'}
-                </Card.Footer>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      )}
+  <p>Loading...</p>
+) : (
+  <Row className="justify-content-center">
+    {/* Mapear a través de la lista de pedidos */}
+    {orders.map((order) => (
+      <Col key={order._id} sm={12} md={4} lg={3} xl={3} className="mb-3">
+        <Card className="card h-100 bg-white rounded">
+          <Card.Header className='cardHeader' >
+            {/* Mostrar la fecha del pedido */}
+          </Card.Header>
+          <Card.Body className='cardBody' >
+            <Card.Title className='cardTitle' >Pedido</Card.Title>
+            <ul>
+              {/* Mapear a través de los productos en el pedido */}
+              {order.products.map((productInfo, index) => (
+                <li key={index}>
+                  <div className="product-info">
+                    {/* Mostrar información del producto */}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </Card.Body>
+          <Card.Footer className='cardFooter' >
+            {/* Mostrar el total del pedido y el estado del pedido */}
+          </Card.Footer>
+        </Card>
+      </Col>
+    ))}
+  </Row>
+)}
     </div>
   );
 }
@@ -99,76 +77,3 @@ function ProfilePage() {
 export default ProfilePage;
 
 
-// import React, { useState, useEffect } from 'react';
-// import authService from "../../services/auth.service";
-// import './ProfilePage.css';
-
-// function ProfilePage() {
-//   const [orders, setOrders] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     async function fetchOrderHistory() {
-//       try {
-//         const response = await authService.api.get("/api/orders/history");
-
-//         setOrders(response.data);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error("Error fetching order history:", error);
-//         setLoading(false);
-//       }
-//     }
-
-//     fetchOrderHistory();
-//   }, []);
-
-//   return (
-//     <div className="profile-page">
-//       <h1>Profile Page</h1>
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <div className="order-list">
-//           {orders.map((order) => (
-//             <div className="order-card" key={order._id}>
-//               <div className="order-details">
-//                 <h3>Order Details</h3>
-//                 <ul>
-//                   {order.products.map((productInfo, index) => (
-//                     <li key={index}>
-//                       <div className="product-info">
-//                         {productInfo.product && (
-//                           <img
-//                             className="product-image"
-//                             src={productInfo.product.imagen}
-//                             alt={productInfo.product.nombre}
-//                           />
-//                         )}
-//                         <div className="product-name">
-//                           {productInfo.amount} x {productInfo.product && productInfo.product.nombre || 'N/A'} (${(productInfo.amount * (productInfo.product && productInfo.product.precio || 0))})
-//                         </div>
-//                       </div>
-//                     </li>
-//                   ))}
-//                 </ul>
-//               </div>
-//               <div className="order-total">
-//                 <strong>Total:</strong> ${calculateTotalPrice(order.products) || 0}
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// // Función para calcular el monto total de la compra
-// function calculateTotalPrice(products) {
-//   return products.reduce((total, productInfo) => {
-//     return total + (productInfo.product ? productInfo.product.precio : 0);
-//   }, 0);
-// }
-
-// export default ProfilePage;
