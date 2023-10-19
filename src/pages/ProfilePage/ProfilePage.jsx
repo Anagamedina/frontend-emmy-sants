f9ce1864fa4c3285badaf6edb7736409f6d5adcc
Ana Gabriela Medina <anaga.medina@gmail.com>
2023-10-19 13:12:27 +0200

    profile ok

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,7 @@
 /*eslint-disable*/
-
 import React, { useState, useEffect } from 'react';
-import authService from "../../services/auth.service";
-import { Link } from "react-router-dom"; // Agrega el import para el componente Link
+import authService from '../../services/auth.service';
+import { Link } from 'react-router-dom';
 import './ProfilePage.css';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
@@ -15,20 +14,12 @@ function ProfilePage() {
   useEffect(() => {
     async function fetchOrderHistory() {
       try {
-        const response = await authService.api.get("/api/orders/history");
-
-        response.data.forEach(order => {
-          let totalAmount = 0;
-          order.products.forEach(productInfo => {
-            totalAmount += productInfo.amount * (productInfo.product ? productInfo.product.precio : 0);
-          });
-          order.totalAmount = totalAmount;
-        });
-
+        const response = await authService.api.get('/api/orders/history');
+       
         setOrders(response.data);
         setLoading(false);
       } catch (error) {
-        console.error("Error fetching order history:", error);
+        console.error('Error fetching order history:', error);
         setLoading(false);
       }
     }
@@ -47,44 +38,38 @@ function ProfilePage() {
           {orders.map((order) => (
             <Col key={order._id} sm={12} md={4} lg={3} xl={3} className="mb-3">
               <Card className="card h-100 bg-white rounded">
-                <Card.Header className='cardHeader' >
-                  <strong>Fecha del Pedido:<br/></strong> {order.createdAt ? new Date(order.createdAt).toLocaleString() : 'N/A'}
+                <Card.Header className="cardHeader">
+                  <strong>Fecha del Pedido:</strong> {order.createdAt ? new Date(order.createdAt).toLocaleString() : 'N/A'}
                 </Card.Header>
-                <Card.Body className='cardBody' >
-                  <Card.Title className='cardTitle' >Pedido</Card.Title>
+                <Card.Body className="cardBody">
+                  <Card.Title className="cardTitle">Pedido</Card.Title>
                   <ul>
                     {order.products.map((productInfo, index) => (
                       <li key={index}>
                         <div className="product-info">
                           {productInfo.product && (
-                            <Link to={`/product/plantas/${productInfo.product._id}`}> 
-                              <img
-                                className="product-image"
-                                src={productInfo.product.imagen}
-                                alt={productInfo.product.nombre}
-                              />
+                            <Link to={`/product/plantas/${productInfo.product._id}`}>
+                              <img className="product-image" src={productInfo.product.imagen} alt={productInfo.product.nombre} />
                             </Link>
                           )}
-                          <div className="product-name">
-                            {productInfo.amount} x{' '}
-                            {productInfo.product && (
-                              <Link to={`/product/plantas/${productInfo.product._id}`}>
-                                <span style={{ textDecoration: 'none', color: '#4abcf1' }}>{productInfo.product.nombre}</span>
-                              </Link>
-                            ) || 'N/A'} 
-                          </div>
-                          <div className="product-price">
-                            Precio por unidad: {productInfo.product && productInfo.product.precio || 0}€
-                          </div>
-                          <div className="product-total">
-                            Total: {productInfo.amount * (productInfo.product && productInfo.product.precio || 0)}€
+                          <div className="product-details">
+                            <div className="product-name">
+                              {productInfo.amount} x{' '}
+                              {productInfo.product && (
+                                <Link to={`/product/plantas/${productInfo.product._id}`}>
+                                  <span style={{ textDecoration: 'none', color: '#4abcf1' }}>{productInfo.product.nombre}</span>
+                                </Link>
+                              ) || 'N/A'}
+                            </div>
+                            <div className="product-price">Precio por unidad: {productInfo.product && productInfo.product.precio || 0}€</div>
+                            <div className="product-total">Total: {productInfo.amount * (productInfo.product && productInfo.product.precio || 0)}€</div>
                           </div>
                         </div>
                       </li>
                     ))}
                   </ul>
                 </Card.Body>
-                <Card.Footer className='cardFooter' >
+                <Card.Footer className="cardFooter">
                   <strong>Total Pedido:</strong> {order.totalAmount || 0}€
                   <br />
                   <strong>Estado del Pedido:</strong> {order.state || 'N/A'}
